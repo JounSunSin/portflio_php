9ffd633b65e6bf2a135e62b73024b8fa7722f797
JongSunShin <bluesky0180@naver.com>
2022-06-09 18:32:54 +0900

    portflio_php

---
+++ index.php
@@ -0,0 +1,221 @@
+<?php require 'view/header.php'; ?>
+    <section id="info_home" class="section">
+        <div class="sp-container">
+            <div class="sp-content">
+                <div class="sp-globe"></div>
+                <h2 class="frame-1">WELCOME MY PORTFOLIO</h2>
+                <h2 class="frame-2">JAVA & PHP & VUE</h2>
+                <h2 class="frame-3">PUBLISHING & FRONT END</h2>
+                <h2 class="frame-4">AND VIDEO EDITING !</h2>
+                <h2 class="frame-5">
+                    <span>PLEASE </span>
+                    <span> CONTACT </span>
+                    <span> EMAIL</span>
+                </h2>
+                <div id="scroll-down" class="scroll-downs">
+                    <div class="mousey">
+                        <div class="scroller"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="info_skill" class="section">
+        <div id="section2_layout">
+            <div class="contactBox">
+                <p class="title_s">
+                    <a class="contact_a">SKILL</a>
+                </p>
+            </div>
+
+            <div id="skillBox">
+                <div class="skill_item s_java" alt="java logo"></div>
+                <div class="skill_item s_php" alt="php logo"></div>
+                <div class="skill_item s_vue" alt="vue logo"></div>
+                <div class="skill_item s_html" alt="html5 logo"></div>
+                <div class="skill_item s_premiere" alt="premiere pro logo"></div>
+                <div class="skill_item s_css" alt="css3 logo"></div>
+                <div class="skill_item s_mysql" alt="mysql logo"></div>
+                <div class="skill_item s_script" alt="javaScript logo"></div>
+                <div class="skill_item s_jquery" alt="jquery logo"></div>
+            </div>
+        </div>
+    </section>
+
+    <section id="info_site" class="section">
+        <div id="section2_layout">
+            <div class="contactBox">
+                <p class="title_s">
+                    <a class="contact_a">Portflio</a>
+                </p>
+            </div>
+            
+            <div class="siteBox">
+                <div id="slider">
+                    <input class="card-slider" type="radio" name="slider" id="s1" checked>
+                    <input class="card-slider" type="radio" name="slider" id="s2">
+                    <input class="card-slider" type="radio" name="slider" id="s3">
+                    <input class="card-slider" type="radio" name="slider" id="s4">
+                    <input class="card-slider" type="radio" name="slider" id="s5">
+                    <input class="card-slider" type="radio" name="slider" id="s6">
+                    <input class="card-slider" type="radio" name="slider" id="s7">
+
+                    <label for="s1" id="slide1" class="card-slider">
+                        <div class="site_flex">
+                            <div class="oneBox site_img1" alt="무한번역"></div>
+                            <div class="twoBox">
+                                <div class="pf_title">무한번역</div>
+                                <div class="pf_lang">PHP</div>
+                                <div class="pf_job">디자인 / 퍼블리싱 / 코딩 </div>
+                                <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                                <div class="pf_btn">
+                                    <button class="btn">
+                                        <a target="_blank" href="http://www.mttrans.co.kr/korean/index.php" alt="무한번역 홈페이지">View Site</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s2" id="slide2" class="card-slider">
+                        <div class="oneBox site_img2" alt="서울대 글로벌리더스"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">GLOBAL LEADERS</div>
+                            <div class="pf_lang">PHP</div>
+                            <div class="pf_job">퍼블리싱 / 포토샵 / 게시판 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn">
+                                <button class="btn">
+                                    <a target="_blank" href="http://globalleaders.snu.ac.kr/korean/" alt="서울대 글로벌 리더스 홈페이지">View Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s3" id="slide3" class="card-slider">
+                        <div class="oneBox site_img3" alt="서울대 정치외교학부"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">서울대 정치외교학부</div>
+                            <div class="pf_lang">PHP</div>
+                            <div class="pf_job">퍼블리싱 / 포토샵 / 게시판 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn btn_multi">
+                                <button class="btn">
+                                    <a target="_blank" href="http://psir.snu.ac.kr/korean/" alt="서울대 정치외교 학부 홈페이지">View Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s4" id="slide4" class="card-slider">
+                        <div class="oneBox site_img4" alt="서울대 정치외교학부"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">서울대 외교학 전공</div>
+                            <div class="pf_lang">PHP</div>
+                            <div class="pf_job">퍼블리싱 / 포토샵 / 게시판 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn btn_multi">
+                                <button class="btn">
+                                    <a target="_blank" href="http://ir.snu.ac.kr/korean/" alt="서울대 외교학 전공 홈페이지">View Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s5" id="slide5" class="card-slider">
+                        <div class="oneBox site_img5" alt="서울대 정치외교학부"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">서울대 정치학 전공</div>
+                            <div class="pf_lang">PHP</div>
+                            <div class="pf_job">퍼블리싱 / 포토샵 / 게시판 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn btn_multi">
+                                <button class="btn">
+                                    <a target="_blank" href="http://polisci.snu.ac.kr/korean/" alt="서울대 정치학 전공 홈페이지">View Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s6" id="slide6" class="card-slider">
+                        <div class="oneBox site_img6" alt="사랑의 장례"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">사랑의 장례</div>
+                            <div class="pf_lang">PHP</div>
+                            <div class="pf_job">기획 / 디자인 / 퍼블리싱 / 코딩 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn">
+                                <button class="btn">
+                                    <a target="_blank" href="https://jopocski.cafe24.com/sun/ex5/korean/index.php?nav=0" alt="portflio">View Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                    <label for="s7" id="slide7" class="card-slider">
+                        <div class="oneBox site_img7" alt="PortFlio"></div>
+                        <div class="twoBox">
+                            <div class="pf_title">PortFlio</div>
+                            <div class="pf_lang">VUE</div>
+                            <div class="pf_job">기획 / 디자인 / 퍼블리싱 / 코딩 </div>
+                            <div class="pf_web">반응형 ( PC / Moblie / Tablet )</div>
+                            <div class="pf_btn">
+                                <button class="btn">
+                                    <a target="_blank" href="https://sun-portflio.netlify.app/" alt="portflio">View VUE</a>
+                                </button>
+                                <button class="btn">
+                                    <a target="_blank" href="https://jopocski.cafe24.com/sun/portflio/index.php" alt="portflio">View PHP</a>
+                                </button>
+                            </div>
+                        </div>
+                    </label>
+                </div> 
+            </div>
+        </div>
+    </section>
+
+    <section id="info_phone" class="section">
+        <div id="section4_layout">
+            <div class="center_layout">
+                <div class="message">
+                    <div class="contactBox">
+                        <p class="title_p">
+                            <a class="contact_a">CONTACT</a>
+                        </p>
+                    </div>
+                    <div class="textBox">
+                        <p class="end_ment_1"></p>
+                        <p class="end_ment_2"></p>
+                        <p class="end_ment_3"></p>
+                        <p class="end_ment_4"></p>
+                        <p class="end_ment_5"></p>
+                    </div>
+                </div>
+
+                <div class="phoneBox">
+                    <div class="selfBox self_address">
+                        <div class="self_img_box">
+                            <img src="img/marker.webp" alt="map" class="self_img_map">
+                        </div>
+                        <div class="self_text">경기도 고양시 화정역</div>
+                    </div>
+                    <div class="selfBox self_email">
+                        <div class="self_img_box">
+                            <img src="img/email.webp" alt="email" class="self_img_email">
+                        </div>
+                        <div class="self_text">bluesky0180@naver.com</div>
+                    </div>
+                    <div class="selfBox self_phone">
+                        <div class="self_img_box">
+                            <img src="img/phone.webp" alt="phone" class="self_img_phone">
+                        </div>
+                        <div class="self_text">010 - 2907 - 0180</div>
+                    </div>
+                </div>           
+            </div>
+
+            <div id="footerDiv">
+                <div>
+                    <a class="self_img_git" href="https://github.com/JounSunSin?tab=repositories"
+                    target="_blank" alt="GitHub"></a>
+                    <div>&copy; 2022 Web Front-end developer by jong sun Shin</div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php require 'view/footer.php'; ?>
---
+++ view/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="HandheldFriendly" content="true">
+        <meta name="format-detection" content="telephone=no" />
+        <meta http-equiv="imagetoolbar" content="no" />
+        <meta name="title" content="JongSun PortFlio">
+        <meta name="description" content="web, moblie, web site, front end, Developer, web publishing">
+        <meta name="keywords" content="web, moblie, web site, front end, Developer, web publishing">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta http-equiv="content-type" content="text/html; charset=euc-kr" />
+
+        <!-- css / script -->
+        <link rel="stylesheet" href="css/main.css" type="text/css" media="all">
+        <script src="js/common.js" type="text/javascript"></script>
+
+        <!-- plugin / font cdn -->
+        <link href="https://fonts.googleapis.com/css2?family=Iceland&family=Noto+Sans+KR:wght@500&display=swap" rel="stylesheet">
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js"></script>
+
+        <title>JongSun PortFlio</title>
+</head>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        window.addEventListener('scroll', () => {
+            this.scroll_num = window.scrollY;
+
+            if (this.scroll_num >= 650) {
+                stateAni("stop");
+            } else if (this.scroll_num < 500) {
+                stateAni("run");
+            }
+        }); 
+
+        backCover();
+        showSkill();
+        typingMsg();
+    });
+</script>
+
+<body>
+    <section>
+        <input type="checkbox" id="main-navigation-toggle" class="btn btn--close" title="Toggle main navigation"/>
+        <label for="main-navigation-toggle" onclick="stateAni('checked')">
+            <span></span>
+        </label>
+
+        <nav id="main-navigation" class="nav-main">
+            <ul class="menu">
+                <li class="menu__item">
+                    <a class="menu__link" onclick="stateAni('move1')" href="#info_home" >HOME</a>
+                </li>
+                <li class="menu__item">
+                    <a class="menu__link" onclick="stateAni('move2')" href="#info_skill" >SKILL</a>
+                </li>
+                <li class="menu__item">
+                    <a class="menu__link" onclick="stateAni('move3')" href="#info_site" >SITE</a>
+                </li>
+                <li class="menu__item">
+                    <a class="menu__link" onclick="stateAni('move4')" href="#info_phone" >PHONE</a>
+                </li>
+            </ul>
+        </nav>
+    </section> 
+
